a262b8fe06bfbe905fadd242288a27d8055441d7
mangeshdhotre <78204994+mangeshdhotre@users.noreply.github.com>
2022-11-16 20:40:48 +0530

    Updates

--- precache-manifest.46e54ae0430a408057a470925bfd0ec7.js
+++ precache-manifest.46e54ae0430a408057a470925bfd0ec7.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "e8e55da6f09502411fc6",
+    "revision": "50ca726f45977c585078",
     "url": "/textutils/static/css/main.bc6b8ce5.chunk.css"
   },
   {
-    "revision": "e8e55da6f09502411fc6",
-    "url": "/textutils/static/js/main.e8e55da6.chunk.js"
+    "revision": "50ca726f45977c585078",
+    "url": "/textutils/static/js/main.50ca726f.chunk.js"
   },
   {
     "revision": "bfa36157bcc75fb8ea07",
     "url": "/textutils/static/js/1.bfa36157.chunk.js"
   },
   {
-    "revision": "c8896b910c0b39bfa1f6",
-    "url": "/textutils/static/js/2.c8896b91.chunk.js"
+    "revision": "0814b22ced1c0c558634",
+    "url": "/textutils/static/js/2.0814b22c.chunk.js"
   },
   {
     "revision": "40124667d252a0e79be6",
     "url": "/textutils/static/js/runtime~main.40124667.js"
   },
   {
-    "revision": "39b72c50b101054cf92e3b875c239b04",
+    "revision": "a9b6a061058f1fad3f45303cd484ce09",
     "url": "/textutils/index.html"
   }
 ];

a155dd518d6465fae913cc6f83a95e859443ebb3
mangeshdhotre <78204994+mangeshdhotre@users.noreply.github.com>
2022-11-16 19:46:09 +0530

    Updates

---
+++ precache-manifest.c84bfa7b12adcf4124b3b98b0ef2e76e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43dd58d615de6c0ff6c3",
+    "url": "/textutils/static/css/main.06b98254.chunk.css"
+  },
+  {
+    "revision": "43dd58d615de6c0ff6c3",
+    "url": "/textutils/static/js/main.43dd58d6.chunk.js"
+  },
+  {
+    "revision": "591bdcbf55c446578560",
+    "url": "/textutils/static/js/1.591bdcbf.chunk.js"
+  },
+  {
+    "revision": "421210603aefa0263cab",
+    "url": "/textutils/static/js/2.42121060.chunk.js"
+  },
+  {
+    "revision": "54be1810a8d090fce527",
+    "url": "/textutils/static/js/runtime~main.54be1810.js"
+  },
+  {
+    "revision": "2ab7f0e8c863b630c9ab896013230664",
+    "url": "/textutils/index.html"
+  }
+];
